7730bfc907726b2348830bc5e00ff88b12b7ed5c
ehime <dodomeki@gmail.com>
2018-11-13 11:55:56 -0600

    All fixes in place

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,18 +8,17 @@ variable default_keypair_public_key {
 
 variable default_keypair_name {
   description = "Name of the KeyPair used for all nodes"
-  default = "k8s-not-the-hardest-way"
+  default     = "k8s-not-the-hardest-way"
 }
-
 
 variable vpc_name {
   description = "Name of the VPC"
-  default = "kubernetes"
+  default     = "kubernetes"
 }
 
 variable elb_name {
   description = "Name of the ELB for Kubernetes API"
-  default = "kubernetes"
+  default     = "kubernetes"
 }
 
 variable owner {
@@ -28,16 +27,16 @@ variable owner {
 
 variable ansibleFilter {
   description = "`ansibleFilter` tag value added to all instances, to enable instance filtering in Ansible dynamic inventory"
-  default = "Kubernetes01" # IF YOU CHANGE THIS YOU HAVE TO CHANGE instance_filters = tag:ansibleFilter=Kubernetes01 in ./ansible/hosts/ec2.ini
+  default     = "Kubernetes01"                                                                                                # IF YOU CHANGE THIS YOU HAVE TO CHANGE instance_filters = tag:ansibleFilter=Kubernetes01 in ./ansible/hosts/ec2.ini
 }
 
 # Networking setup
 variable region {
-  default = "eu-west-1"
+  default = "us-east-1"
 }
 
 variable zone {
-  default = "eu-west-1a"
+  default = "us-east-1a"
 }
 
 ### VARIABLES BELOW MUST NOT BE CHANGED ###
@@ -50,23 +49,24 @@ variable kubernetes_pod_cidr {
   default = "10.200.0.0/16"
 }
 
-
 # Instances Setup
 variable amis {
   description = "Default AMIs to use for nodes depending on the region"
-  type = "map"
+  type        = "map"
+
   default = {
     ap-northeast-1 = "ami-0567c164"
     ap-southeast-1 = "ami-a1288ec2"
-    cn-north-1 = "ami-d9f226b4"
-    eu-central-1 = "ami-8504fdea"
-    eu-west-1 = "ami-0d77397e"
-    sa-east-1 = "ami-e93da085"
-    us-east-1 = "ami-40d28157"
-    us-west-1 = "ami-6e165d0e"
-    us-west-2 = "ami-a9d276c9"
+    cn-north-1     = "ami-d9f226b4"
+    eu-central-1   = "ami-8504fdea"
+    us-east-1      = "ami-0d77397e"
+    sa-east-1      = "ami-e93da085"
+    us-east-1      = "ami-40d28157"
+    us-west-1      = "ami-6e165d0e"
+    us-west-2      = "ami-a9d276c9"
   }
 }
+
 variable default_instance_user {
   default = "ubuntu"
 }
@@ -74,14 +74,26 @@ variable default_instance_user {
 variable etcd_instance_type {
   default = "t2.small"
 }
+
 variable controller_instance_type {
   default = "t2.small"
 }
+
 variable worker_instance_type {
   default = "t2.small"
 }
 
-
 variable kubernetes_cluster_dns {
   default = "10.31.0.1"
+}
+
+resource "aws_vpc" "kubernetes" {
+  cidr_block           = "10.43.0.0/16"
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "kubernetes" {
+  vpc_id            = "${aws_vpc.kubernetes.id}"
+  cidr_block        = "10.43.0.0/16"
+  availability_zone = "eu-west-1a"
 }
